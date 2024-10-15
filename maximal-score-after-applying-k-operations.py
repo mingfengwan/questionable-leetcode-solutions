89fdcf49721ac5bc6766132efe415deba21309ee
mingfengwan <ming.wan@mail.utoronto.ca>
2024-10-14 22:39:13 -0400

    Update maximal-score-after-applying-k-operations.py
    remove unnecessary heapify step

--- maximal-score-after-applying-k-operations.py
+++ maximal-score-after-applying-k-operations.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
-        h = [-x for x in sorted(nums)[-k:]]
-        heapq.heapify(h)
+        h = [-x for x in sorted(nums, reverse=True)[:k]]
         ans = 0
 
         for _ in range(k):
