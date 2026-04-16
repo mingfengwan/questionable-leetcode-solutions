9f46fd8f19f07ec79d4c9d3c549d98de4361e701
mingfengwan <wmf410@outlook.com>
2026-04-15 22:11:05 -0700

    Update equal-sum-grid-partition.py

--- equal-sum-grid-partition.py
+++ equal-sum-grid-partition.py
@@ -1,33 +1,36 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        m = len(grid)
-        n = len(grid[0])
+        m, n = len(grid), len(grid[0])
 
         sums_row = [0] * m
         sums_col = [0] * n
+        
+        total = 0
 
         for i in range(m):
             for j in range(n):
                 sums_row[i] += grid[i][j]
                 sums_col[j] += grid[i][j]
+                total += grid[i][j]
+        
+        if total % 2 != 0:
+            return False
+        
+        half = total // 2
         
         def can_split_equally(arr, length):
-            i = 0
-            j = length - 1
-            left = arr[i]
-            right = arr[j]
+            cur = 0
 
-            while i < j - 1:
-                if left > right:
-                    j -= 1
-                    right += arr[j]
-                else:
-                    i += 1
-                    left += arr[i]
+            for i in range(length - 1):
+                cur += arr[i]
+                if cur == half:
+                    return True
+                elif cur > half:
+                    break
 
-            return left == right
+            return False
         
-        if m > 1 and can_split_equally(sums_row, m):
+        if can_split_equally(sums_row, m):
             return True
 
-        return n > 1 and can_split_equally(sums_col, n)
+        return can_split_equally(sums_col, n)
